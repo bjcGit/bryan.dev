044fb5f38b56c69455430363d8ffce3558949a00
bjcGit <48102680+bjcGit@users.noreply.github.com>
2025-01-30 18:12:33 -0500

    Update ProfileInfo.tsx
    adicion de info

--- src/components/ProfileInfo/ProfileInfo.tsx
+++ src/components/ProfileInfo/ProfileInfo.tsx
@@ -5,7 +5,7 @@ export const ProfileInfo = () => {
       <p className="text-sm sm:text-base text-green-400 mt-2">Desarrollador <span className="font-bold uppercase">FullStack</span></p>
       <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 my-6">
         <div className="text-center">
-          <h3 className="font-semibold text-green-500 text-lg">4</h3>
+          <h3 className="font-semibold text-green-500 text-lg">7</h3>
           <p className="text-green-400 text-sm">Proyectos</p>
         </div>
         <div className="text-center">
@@ -13,7 +13,7 @@ export const ProfileInfo = () => {
           <p className="text-green-400 text-sm">Empresas</p>
         </div>
         <div className="text-center">
-          <h3 className="font-semibold text-green-500 text-lg">4</h3>
+          <h3 className="font-semibold text-green-500 text-lg">5</h3>
           <p className="text-green-400 text-sm">Despliegues</p>
         </div>
         <div className="text-center">
